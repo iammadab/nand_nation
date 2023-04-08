2facc1ee017c8cbfa383f61c2d0add2cc5a19115
Wisdom Ogwu <iammadab@gmail.com>
2023-04-08 21:03:51 +0100

    implement the nand gate from the truth table

---
+++ src/chips/nand.rs
@@ -0,0 +1,25 @@
+use crate::bit::Bit;
+use crate::bit::Bit::{One, Zero};
+
+// Primitive gate, as such we implement the truth table directly
+fn nand(a: Bit, b: Bit) -> Bit {
+    match (a, b) {
+        (Zero, Zero) => One,
+        (Zero, One) => One,
+        (One, Zero) => One,
+        (One, One) => Zero,
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn nand_gate() {
+        assert_eq!(nand(Zero, Zero), One);
+        assert_eq!(nand(Zero, One), One);
+        assert_eq!(nand(One, Zero), One);
+        assert_eq!(nand(One, One), Zero);
+    }
+}
