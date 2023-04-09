0e554d98dbd5eac08df2adb894b2e15f28a9e283
Wisdom Ogwu <iammadab@gmail.com>
2023-04-09 19:25:04 +0100

    Make Bit16 generic over 16

--- src/bit.rs
+++ src/bit.rs
@@ -7,31 +7,31 @@ pub(crate) enum Bit {
 }
 
 #[derive(Debug, PartialEq)]
-pub(crate) struct Bit16(pub(crate) [Bit; 16]);
+pub(crate) struct BitN<const N: usize>([Bit; N]);
 
-impl Default for Bit16 {
+impl<const N: usize> Default for BitN<N> {
     fn default() -> Self {
-        Bit16([Bit::Zero; 16])
+        BitN([Bit::Zero; N])
     }
 }
 
-impl Index<usize> for Bit16 {
+impl<const N: usize> Index<usize> for BitN<N> {
     type Output = Bit;
     fn index(&self, index: usize) -> &Self::Output {
         &self.0[index]
     }
 }
 
-impl IndexMut<usize> for Bit16 {
+impl<const N: usize> IndexMut<usize> for BitN<N> {
     fn index_mut(&mut self, index: usize) -> &mut Self::Output {
         &mut self.0[index]
     }
 }
 
-impl From<String> for Bit16 {
-    fn from(value: String) -> Bit16 {
-        debug_assert_eq!(value.len(), 16);
-        let mut result = Bit16::default();
+impl<const N: usize> From<String> for BitN<N> {
+    fn from(value: String) -> BitN<N> {
+        debug_assert_eq!(value.len(), N);
+        let mut result: BitN<N> = BitN::default();
         for (i, char) in value.chars().enumerate() {
             match char {
                 '1' => result[i] = Bit::One,
@@ -42,6 +42,14 @@ impl From<String> for Bit16 {
     }
 }
 
+impl<const N: usize> From<[Bit; N]> for BitN<N> {
+    fn from(value: [Bit; N]) -> BitN<N> {
+        BitN::<N>(value)
+    }
+}
+
+pub(crate) type Bit16 = BitN<16>;
+
 #[cfg(test)]
 mod test {
     use crate::bit::Bit::{One, Zero};
@@ -50,13 +58,13 @@ mod test {
     #[test]
     fn bit16_from_string() {
         let bits = Bit16::from(String::from("0000000000000000"));
-        assert_eq!(bits, Bit16([Zero; 16]));
+        assert_eq!(bits, Bit16::from([Zero; 16]));
         let bits = Bit16::from(String::from("1111111111111111"));
-        assert_eq!(bits, Bit16([One; 16]));
+        assert_eq!(bits, Bit16::from([One; 16]));
         let bits = Bit16::from(String::from("1010101010101010"));
         assert_eq!(
             bits,
-            Bit16([
+            Bit16::from([
                 One, Zero, One, Zero, One, Zero, One, Zero, One, Zero, One, Zero, One, Zero, One,
                 Zero
             ])
