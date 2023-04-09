a5b04fbcd05d8d64c1e377e30446e4d5a1ee9bf0
Wisdom Ogwu <iammadab@gmail.com>
2023-04-09 14:31:41 +0100

    Create macro fot and16 test

--- src/chips/and.rs
+++ src/chips/and.rs
@@ -22,6 +22,15 @@ mod test {
     use crate::bit::{Bit, Bit16};
     use crate::chips::and::{and, and16};
 
+    macro_rules! and16test {
+        ($a:expr, $b:expr, $out:expr) => {
+            assert_eq!(
+                and16(Bit16::from(String::from($a)), Bit16::from(String::from($b))),
+                Bit16::from(String::from($out))
+            )
+        };
+    }
+
     #[test]
     fn and_gate() {
         assert_eq!(and(Zero, Zero), Zero);
@@ -32,47 +41,11 @@ mod test {
 
     #[test]
     fn and16_gate() {
-        assert_eq!(
-            and16(
-                Bit16::from(String::from("0000000000000000")),
-                Bit16::from(String::from("0000000000000000"))
-            ),
-            Bit16::from(String::from("0000000000000000"))
-        );
-        assert_eq!(
-            and16(
-                Bit16::from(String::from("0000000000000000")),
-                Bit16::from(String::from("1111111111111111"))
-            ),
-            Bit16::from(String::from("0000000000000000"))
-        );
-        assert_eq!(
-            and16(
-                Bit16::from(String::from("1111111111111111")),
-                Bit16::from(String::from("1111111111111111"))
-            ),
-            Bit16::from(String::from("1111111111111111"))
-        );
-        assert_eq!(
-            and16(
-                Bit16::from(String::from("1010101010101010")),
-                Bit16::from(String::from("0101010101010101"))
-            ),
-            Bit16::from(String::from("0000000000000000"))
-        );
-        assert_eq!(
-            and16(
-                Bit16::from(String::from("0011110011000011")),
-                Bit16::from(String::from("0000111111110000"))
-            ),
-            Bit16::from(String::from("0000110011000000"))
-        );
-        assert_eq!(
-            and16(
-                Bit16::from(String::from("0001001000110100")),
-                Bit16::from(String::from("1001100001110110"))
-            ),
-            Bit16::from(String::from("0001000000110100"))
-        );
+        and16test!("0000000000000000", "0000000000000000", "0000000000000000");
+        and16test!("0000000000000000", "1111111111111111", "0000000000000000");
+        and16test!("1111111111111111", "1111111111111111", "1111111111111111");
+        and16test!("1010101010101010", "0101010101010101", "0000000000000000");
+        and16test!("0011110011000011", "0000111111110000", "0000110011000000");
+        and16test!("0001001000110100", "1001100001110110", "0001000000110100");
     }
 }
