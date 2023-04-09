bea80a7543f91586a84c4cd8d316e6b38aacaa44
Wisdom Ogwu <iammadab@gmail.com>
2023-04-09 12:35:22 +0100

    Implement or16 gate

--- src/chips/or.rs
+++ src/chips/or.rs
@@ -1,4 +1,4 @@
-use crate::bit::Bit;
+use crate::bit::{Bit, Bit16};
 use crate::chips::and::and;
 use crate::chips::not::not;
 
@@ -14,6 +14,14 @@ pub(crate) fn or(a: Bit, b: Bit) -> Bit {
     not(and(not(a), not(b)))
 }
 
+pub(crate) fn or16(a: Bit16, b: Bit16) -> Bit16 {
+    let mut result = Bit16::default();
+    for i in 0..16 {
+        result[i] = or(a[i], b[i])
+    }
+    result
+}
+
 #[cfg(test)]
 mod test {
     use super::*;
@@ -25,5 +33,51 @@ mod test {
         assert_eq!(or(Zero, One), One);
         assert_eq!(or(One, Zero), One);
         assert_eq!(or(One, One), One);
+    }
+
+    #[test]
+    fn or16_gate() {
+        assert_eq!(
+            or16(
+                Bit16::from(String::from("0000000000000000")),
+                Bit16::from(String::from("0000000000000000"))
+            ),
+            Bit16::from(String::from("0000000000000000"))
+        );
+        assert_eq!(
+            or16(
+                Bit16::from(String::from("0000000000000000")),
+                Bit16::from(String::from("1111111111111111"))
+            ),
+            Bit16::from(String::from("1111111111111111"))
+        );
+        assert_eq!(
+            or16(
+                Bit16::from(String::from("1111111111111111")),
+                Bit16::from(String::from("1111111111111111"))
+            ),
+            Bit16::from(String::from("1111111111111111"))
+        );
+        assert_eq!(
+            or16(
+                Bit16::from(String::from("1010101010101010")),
+                Bit16::from(String::from("0101010101010101"))
+            ),
+            Bit16::from(String::from("1111111111111111"))
+        );
+        assert_eq!(
+            or16(
+                Bit16::from(String::from("0011110011000011")),
+                Bit16::from(String::from("0000111111110000"))
+            ),
+            Bit16::from(String::from("0011111111110011"))
+        );
+        assert_eq!(
+            or16(
+                Bit16::from(String::from("0001001000110100")),
+                Bit16::from(String::from("1001100001110110"))
+            ),
+            Bit16::from(String::from("1001101001110110"))
+        );
     }
 }
