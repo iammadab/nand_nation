1dfc274761b2ae9152817ed1c9dfd316f9348c27
Wisdom Ogwu <iammadab@gmail.com>
2023-04-30 10:14:00 +0100

    add test for register

--- src/chips/memory/register.rs
+++ src/chips/memory/register.rs
@@ -28,6 +28,25 @@ impl Register {
 
 #[cfg(test)]
 mod tests {
+    use crate::bit::Bit;
+    use crate::chips::memory::register::Register;
+    use crate::testing::TestReader;
+
     #[test]
-    fn register_gate() {}
+    fn register_gate() {
+        let mut register = Register::new();
+
+        let test_tokens = TestReader::read("register.txt");
+        let mut token_iter = test_tokens.into_iter();
+        // skip the header
+        let mut token_iter = token_iter.skip(4);
+
+        // continue as long as we have clock input
+        while token_iter.next().is_some() {
+            let input = TestReader::from_16_bit_int_string(token_iter.next().unwrap());
+            let load = Bit::from(token_iter.next().unwrap());
+            let out = TestReader::from_16_bit_int_string(token_iter.next().unwrap());
+            assert_eq!(register.clock(input, load), out);
+        }
+    }
 }
